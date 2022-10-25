6d80460b225b0fee201d34a994e071e4a58fe323
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-10-25 20:59:20 +0200

    section 3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,7 +14,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
           également de répondre à un appel, de vous rappeler d’un événement de votre agenda, ou de vous donner la météo. Elle
           suivra votre activité physique quotidienne et votre fréquence cardiaque en temps réel.
         </p>
-        <p class="font-MerriweatherSans text-lg mt-3">
+        <p class="font-MerriweatherSans text-lg mt-4">
           Elle vous accompagnera partout grâce à son design sobre et élégant !
         </p>
       </div>
@@ -36,5 +36,21 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
       <ChevronRightIcon class="w-10 text-white"></ChevronRightIcon>
     </div>
     <a class="text-white text-center underline text-lg" href=""><p>Voir plus de mes personnalisations</p></a>
+  </section>
+
+  <!-- Section 3 -->
+  <section>
+    <div class="flex">
+      <div>
+        <img src="/public/MontreBeige.webp" alt="Montre">
+      </div>
+      <div class="font-MerriweatherSans text-lg flex flex-col place-content-center">
+        <p class="my-3">Cette montre connectée personnalisable est universelle et unisexe, et elle fonctionne avec tous les systèmes
+        d’exploitation existant !</p>
+        <p class="my-3">L’autonomie de cette montre est variable, entre 4 à 6 jours selon l’utilisation !</p>
+        <p class="my-3">En plus de posséder les fonctions d’ECG et de Cardiofréquencemètre, cette montre est étanche jusqu’à 1m de profondeur
+        pendant 30 min, grâce à la norme IP67 !</p>
+      </div>
+    </div>
   </section>
 </template>
