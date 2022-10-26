3da6b664b722403a2b768258a765881003e952cb
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-10-26 16:53:58 +0200

    page enregister

--- src/pages/enregistrer.vue
+++ src/pages/enregistrer.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-
+import ListeMontre from '@/components/ListeMontre.vue';
 
 </script>
 
 <template >
     <div class="bg-GrisMoyen grid grid-cols-3">
-        
+        <ListeMontre/>
     </div>
 </template>
