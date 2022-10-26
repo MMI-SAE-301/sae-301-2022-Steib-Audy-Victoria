06281d6f73b9beca8230627e40910563a56f73bc
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-10-26 16:44:26 +0200

    correction liste

--- src/components/ListeMontre.vue
+++ src/components/ListeMontre.vue
@@ -15,8 +15,8 @@ if (error) {
 </script>
 <template>
     <ul>
-        <li v-for="montre in montres" :key="montre.id">
-            <router-link :to="{ name: 'montre-edit-id', params: { id: montre.id } }">
+        <li v-for="montre in montres" :key="montre.Id">
+            <router-link :to="{ name: 'montre-edit-id', params: { id: montre.Id } }">
                 <montreCarree class="w-64" v-bind="montre" />
             </router-link>
         </li>
