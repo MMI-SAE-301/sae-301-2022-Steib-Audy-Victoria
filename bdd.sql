a0c7ba6f140262fd8bfc4ba4afb119b8b4b3848b
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-10-27 14:23:12 +0200

    remplir bdd

--- bdd.sql
+++ bdd.sql
@@ -1,21 +1,53 @@
 --
 -- code pour la création des tables
 --
+CREATE TABLE Montre (
+    id int,
+    commande boolean,
+    boitier varchar,
+    ecran varchar, 
+    id_user int, 
+    id_mat_boitier int,
+    id_mat_bracelet int,
+    bracelet varchar,
+    texte varchar,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_user) references auth,
+    FOREIGN KEY (id_mat_boitier) references Matériaux,
+    FOREIGN KEY (id_mat_bracelet) references Matériaux
 
+)
 
-
-
-
-
-
+CREATE TABLE Matériaux (
+    id int,
+    Libelle varchar,
+    PRIMARY KEY (id)
+)
 
 --
 -- code pour la création des vues
 --
 
+CREATE VIEW allCuirs as
+select "Montre".*
+from "Matériaux", "Montre"
+where "Matériaux"."Id" = "Montre".id_mat_boitier or "Matériaux"."Id" = "Montre".id_mat_bracelet
+and "Matériaux"."Libelle" = 'Cuir';
+
+CREATE VIEW allMatériaux as
+SELECT *
+FROM "Matériaux";
+
+create view allMontre as
+SELECT *
+FROM "Montre";
 
 
 
 --
 -- code pour la création des policies
 --
+policies insert :
+(uid() = id_user)
+(uid() IN ( SELECT allMontre.id_user
+   FROM allMontre))
